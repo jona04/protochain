48b43a5b0d9ba7b374cc43db2ef5f36badf5c6e4
Jonatas <jonatas.iw@gmail.com>
2025-08-21 15:25:18 -0300

    add hash in blocks

--- __tests__/block.test.ts
+++ __tests__/block.test.ts
@@ -10,12 +10,35 @@ describe("Teste", () => {
     })
 
     it('Block is invalid', () => {
-        let myBlock = new Block(-1,"1")
+        let myBlock = new Block(-1,"previous abc", "data block 2")
         expect(myBlock.isValid()).toBeFalsy()
     })
 
     it('Block is valid', () => {
-        let myBlock = new Block(2,"1")
+        let myBlock = new Block(2,"previous a", "data block 3")
         expect(myBlock.isValid()).toBeTruthy()
     })
+
+    it('Block hash is not valid', () => {
+        let myBlock = new Block(2, "previous block", "Test")
+        myBlock.hash = "";
+        expect(myBlock.isValid()).toBeFalsy()
+    })
+
+    it('Block data is not valid', () => {
+        let myBlock = new Block(2, "previous block", "")
+        expect(myBlock.isValid()).toBeFalsy()
+    })
+
+    it('Block previousData is not valid', () => {
+        let myBlock = new Block(2, "", "Data test")
+        expect(myBlock.isValid()).toBeFalsy()
+    })
+
+    it('Block timestamp is not valid', () => {
+        let myBlock = new Block(2, "Previous hash", "Data test")
+        myBlock.timestamp = 0;
+        expect(myBlock.isValid()).toBeFalsy()
+    })
+
 })
