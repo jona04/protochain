28eabdb997e5791ad2137a7a57915d00848c720d
Jonatas <jonatas.iw@gmail.com>
2025-08-22 14:14:48 -0300

    add miner

--- __tests__/block.test.ts
+++ __tests__/block.test.ts
@@ -2,6 +2,8 @@ import Block from "../src/lib/block"
 
 describe("Teste", () => {
 
+    const exampleDifficulty: number = 0;
+    const exampleMiner: string = "jonatas";
     let genesis: Block;
 
     beforeAll(() => {
@@ -12,12 +14,13 @@ describe("Teste", () => {
         let myBlock = new Block(
             {index: 1, previousHash: genesis.hash, data:"Block 2"} as Block
         )
-        expect(myBlock.isValid(genesis.hash, genesis.index).success).toBeTruthy()
+        myBlock.mine(exampleDifficulty, exampleMiner);
+        expect(myBlock.isValid(genesis.hash, genesis.index, exampleDifficulty).success).toBeTruthy()
     })
 
     it('Block is valid (fallbacks)', () => {
         let myBlock = new Block()
-        expect(myBlock.isValid(genesis.hash, genesis.index).success).toBeFalsy()
+        expect(myBlock.isValid(genesis.hash, genesis.index, exampleDifficulty).success).toBeFalsy()
     })
 
     it('Block hash is not valid', () => {
@@ -25,21 +28,31 @@ describe("Teste", () => {
             {index: 1, previousHash: genesis.hash, data:"Block 2"} as Block
         )
         myBlock.hash = "";
-        expect(myBlock.isValid(genesis.hash, genesis.index).success).toBeFalsy()
+        expect(myBlock.isValid(genesis.hash, genesis.index, exampleDifficulty).success).toBeFalsy()
+    })
+
+    it('Block hash is not valid (miner)', () => {
+        let myBlock = new Block(
+            {index: 1, previousHash: genesis.hash, data:"Block 2"} as Block
+        )
+        myBlock.mine(exampleDifficulty, exampleMiner);
+        
+        myBlock.hash = "";
+        expect(myBlock.isValid(genesis.hash, genesis.index, exampleDifficulty).success).toBeFalsy()
     })
 
     it('Block data is not valid', () => {
         let myBlock = new Block(
             {index: 1, previousHash: genesis.hash, data:""} as Block
         )
-        expect(myBlock.isValid(genesis.hash, genesis.index).success).toBeFalsy()
+        expect(myBlock.isValid(genesis.hash, genesis.index, exampleDifficulty).success).toBeFalsy()
     })
 
     it('Block previousHash is not valid', () => {
         let myBlock = new Block(
             {index: 1, previousHash: "Wrong", data:"Block 2"} as Block
         )
-        expect(myBlock.isValid(genesis.hash, genesis.index).success).toBeFalsy()
+        expect(myBlock.isValid(genesis.hash, genesis.index, exampleDifficulty).success).toBeFalsy()
     })
 
     it('Block timestamp is not valid', () => {
@@ -48,8 +61,7 @@ describe("Teste", () => {
         )
         myBlock.timestamp = -1;
         myBlock.hash = myBlock.getHash();
-        const validation = myBlock.isValid(genesis.hash, genesis.index)
+        const validation = myBlock.isValid(genesis.hash, genesis.index, exampleDifficulty)
         expect(validation.success).toBeFalsy()
     })
-  
 })
