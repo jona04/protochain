73bbdc2549360e108cba63b78b2623c2b5f449e3
Jonatas <jonatas.iw@gmail.com>
2025-08-22 09:02:41 -0300

    add blockchain server

--- __tests__/block.test.ts
+++ __tests__/block.test.ts
@@ -2,43 +2,45 @@ import Block from "../src/lib/block"
 
 describe("Teste", () => {
 
-    it('sould be true', () => {
-        
-        const valid = true
+    let genesis: Block;
 
-        expect(valid).toEqual(true)
+    beforeAll(() => {
+        genesis = new Block(0, "my hash", "genesis block")
     })
 
     it('Block is invalid', () => {
-        let myBlock = new Block(-1,"previous abc", "data block 2")
-        expect(myBlock.isValid()).toBeFalsy()
+        let myBlock = new Block(-1,genesis.hash, "data block 2")
+        expect(myBlock.isValid(genesis.hash, genesis.index).success).toBeFalsy()
     })
 
     it('Block is valid', () => {
-        let myBlock = new Block(2,"previous a", "data block 3")
-        expect(myBlock.isValid()).toBeTruthy()
+        let myBlock = new Block(1,genesis.hash, "data block 3")
+        expect(myBlock.isValid(genesis.hash, genesis.index).success).toBeTruthy()
     })
 
     it('Block hash is not valid', () => {
-        let myBlock = new Block(2, "previous block", "Test")
+        let myBlock = new Block(1, genesis.hash, "Test")
         myBlock.hash = "";
-        expect(myBlock.isValid()).toBeFalsy()
+        expect(myBlock.isValid(genesis.hash, genesis.index).success).toBeFalsy()
     })
 
     it('Block data is not valid', () => {
-        let myBlock = new Block(2, "previous block", "")
-        expect(myBlock.isValid()).toBeFalsy()
+        let myBlock = new Block(1, genesis.hash, "")
+        expect(myBlock.isValid(genesis.hash, genesis.index).success).toBeFalsy()
     })
 
-    it('Block previousData is not valid', () => {
-        let myBlock = new Block(2, "", "Data test")
-        expect(myBlock.isValid()).toBeFalsy()
+    it('Block previousHash is not valid', () => {
+        let myBlock = new Block(1, "wrong previous", "Data test")
+        expect(myBlock.isValid(genesis.hash, genesis.index).success).toBeFalsy()
     })
 
     it('Block timestamp is not valid', () => {
-        let myBlock = new Block(2, "Previous hash", "Data test")
-        myBlock.timestamp = 0;
-        expect(myBlock.isValid()).toBeFalsy()
+        let myBlock = new Block(1, genesis.hash, "Data test")
+        myBlock.timestamp = -1;
+        myBlock.hash = myBlock.getHash();
+        const validation = myBlock.isValid(genesis.hash, genesis.index)
+        console.log("validation", validation.message);
+        expect(validation.success).toBeFalsy()
     })
-
+  
 })
--- __tests__/blockchain.test.ts
+++ __tests__/blockchain.test.ts
@@ -1,4 +1,5 @@
-import Blockchain from '../src/lib/blockchain'
+import Blockchain from '../src/lib/blockchain';
+import Block from '../src/lib/block';
 
 describe("Blockchain tests", () => {
 
@@ -7,4 +8,61 @@ describe("Blockchain tests", () => {
         expect(blockchain.blocks.length).toEqual(1);
     })
 
+    test('Should be valid', () =>{
+        const blockchain = new Blockchain();
+        expect(blockchain.isValid().success).toEqual(true);
+    })
+
+    test('Should be valid (two blocks)', () =>{
+        const blockchain = new Blockchain();
+        blockchain.addBlock(new Block(1, blockchain.getLastBlock().hash, "Block 2"));
+        blockchain.addBlock(new Block(2, blockchain.getLastBlock().hash, "Block 3"));
+        expect(blockchain.isValid().success).toEqual(true);
+    })
+
+    test('Should NOT be valid (two blocks)', () =>{
+        const blockchain = new Blockchain();
+        const last = blockchain.getLastBlock();
+        
+        const b2 = new Block(last.index + 1, last.hash, 'B2');
+        blockchain.addBlock(b2);
+        
+        (blockchain as any).blocks[1] = undefined;
+
+        expect(() => blockchain.isValid()).toThrow('Block not found during blockchain validation!');
+    })
+
+    test('Should NOT be valid', () =>{
+        const blockchain = new Blockchain();
+        blockchain.addBlock(new Block(1, blockchain.getLastBlock().hash, "Block 2"));
+        const block = blockchain.getLastBlock();
+        block.data = "a transfere 2 para B";
+        expect(blockchain.isValid().success).toEqual(false);
+    })
+
+    test('Should add block', () =>{
+        const blockchain = new Blockchain();
+        blockchain.addBlock(new Block(1, blockchain.blocks[0].hash, "Block 2"));
+        expect(blockchain.isValid().success).toEqual(true);
+    })
+
+    test('Should NOT add block', () =>{
+        const blockchain = new Blockchain();
+        const block = new Block(-1, blockchain.blocks[0].hash, "Block 2");
+        const result = blockchain.addBlock(block);
+        expect(result.success).toEqual(false);
+    })
+    
+    test('Should get block', () =>{
+        const blockchain = new Blockchain();
+        const block = blockchain.getBlock(blockchain.blocks[0].hash);
+        const result = blockchain.isValid();
+        expect(result.success).toEqual(true);
+    })
+
+    test('getLastBlock with error', () => {
+        const bc = new Blockchain();
+        (bc as any).blocks = [] as any;
+        expect(() => bc.getLastBlock()).toThrow('Empty blockchain');
+    });
 })
