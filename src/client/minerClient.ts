a4826399bdb04819a068a3d493400bf6dc9e419f
Jonatas <jonatas.iw@gmail.com>
2025-08-24 22:09:04 -0300

    adjustments on miner

--- src/client/minerClient.ts
+++ src/client/minerClient.ts
@@ -18,6 +18,12 @@ let totalMined = 0;
 async function mine(){
     console.log("Getting next block info...");
     const {data} = await axios.get(`${BLOCKCHAIN_SERVER}blocks/next`);
+    if(!data) {
+        console.log("No tx found. Waiting....");
+        return setTimeout(()=>{
+            mine();
+        }, 5000)
+    }
     const blockinfo = data as BlockInfo;
     
     const newBLock = Block.fromBlockInfo(blockinfo)
